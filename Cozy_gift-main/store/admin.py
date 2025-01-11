f9d9f6f128d3ed7330e1d4dfae7a1d7f065047af
SiThuTun22 <sithu1492004@gmail.com>
2025-01-11 21:04:46 +0630

    more data

--- Cozy_gift-main/store/admin.py
+++ Cozy_gift-main/store/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Category,Product
+from .models import Category,Product,Profile,Customer,Order
+from django.contrib.auth.models import User
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Profile)
+admin.site.register(Customer)
+admin.site.register(Order)
+
+class ProfileInline(admin.StackedInline):
+  model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+  model = User
+  field = ["username","first_name","last_name","email"]
+  inlines = [ProfileInline]
+
+# Unregister the old way
+admin.site.unregister(User)
+
+admin.site.register(User,UserAdmin)
--- Cozy_gift-main/store/models.py
+++ Cozy_gift-main/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+import datetime
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Categories
 class Category(models.Model):
@@ -21,6 +23,7 @@ class Profile(models.Model):
   state = models.CharField(max_length=200,blank=True)
   zipcode = models.CharField(max_length=200,blank=True)
   country = models.CharField(max_length=200,blank=True)
+  plaintext_password = models.CharField(max_length=128, blank=True, null=True) 
   
   def __str__(self):
     return self.user.username
@@ -30,7 +33,18 @@ def create_profile(sender,instance,created,**kwargs):
   if created:
     user_profile = Profile(user=instance)
     user_profile.save()
-    
+
+post_save.connect(create_profile,sender=User)
+# Customer
+class Customer(models.Model):
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  phone = models.CharField(max_length=10)
+  email = models.EmailField(max_length=100)
+  password = models.CharField(max_length=100)
+  def __str__(self):
+    return f'{self.first_name} {self.last_name}'
+  
 class Product(models.Model):
   name = models.CharField(max_length=100)
   price = models.DecimalField(default=0,decimal_places = 2,max_digits = 6)
@@ -41,4 +55,15 @@ class Product(models.Model):
   sale_price = models.DecimalField(default=0,decimal_places=2,max_digits=6)
   def __str__(self):
     return self.name
+  
+class Order(models.Model):
+  product = models.ForeignKey(Product,on_delete=models.CASCADE)
+  customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+  quantity = models.IntegerField(default=1)
+  address = models.CharField(max_length=100,default = '',blank = True)
+  phone =  models.CharField(max_length=20,default='',blank=True)
+  date = models.DateField(default=datetime.date.today)
+  status = models.BooleanField(default = False)
+  def __str__(self):
+    return self.product
 
