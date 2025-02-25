bf42079750aa8635343cbc1f5c088af6a50749f0
SiThuTun22 <sithu1492004@gmail.com>
2025-02-25 16:50:16 +0630

    add billing_info

--- Cozy_gift-main/payment/forms.py
+++ Cozy_gift-main/payment/forms.py
@@ -16,10 +16,14 @@ class ShippingForm(forms.ModelForm):
 
     exclude = ['user',]
 
-# class PaymentForm(forms.Form):
-#   card_name = 
-#   cart_number = 
-#   cart_exp_date = 
-#   card_cvv_number = 
-#   card_address1 = 
-#   card_address2 = 
+class PaymentForm(forms.Form):
+  card_name = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Name on Card'}),required=True)
+  cart_number = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Card Number'}),required=True)
+  cart_exp_date = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Expiration Date'}),required=True)
+  card_cvv_number = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'CVV code'}),required=True)
+  card_address1 = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Billing Address 1'}),required=True)
+  card_address2 =  forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Billing Address 2'}),required=False)
+  card_city = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Billing City'}),required=True)
+  card_state = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Billing State'}),required=True)
+  card_zipcode = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Billing Zipcode'}),required=True)
+  card_country = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Billing Country'}),required=True)
